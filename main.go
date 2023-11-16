6eadbd0f9103b60d620d563475b4f62e20b9bc48
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-11-16 00:22:18 +0000

    Added logging

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Println("Request received")
 		io.WriteString(w, "Hello from a HandleFunc #1!\n")
 	}
 	http.HandleFunc("/", h1)
+	fmt.Println("Starting server")
 	fmt.Println(http.ListenAndServe("localhost:8080", nil))
 }
