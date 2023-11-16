126ce21548c248875e10a1ca5527eecb86460944
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-11-16 00:34:19 +0000

    using unix sockets

--- main.go
+++ main.go
@@ -7,11 +7,27 @@ import (
 )
 
 func main() {
+	socket, err := net.Listen("unix", "/~/conn.sock")
+	if err != nil {
+		panic(err)
+	}
+
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Println("Request received")
 		io.WriteString(w, "Hello from a HandleFunc #1!\n")
 	}
-	http.HandleFunc("/", h1)
-	fmt.Println("Starting server")
-	fmt.Println(http.ListenAndServe("localhost:8080", nil))
+
+	m := http.NewServeMux()
+	m.HandleFunc("/", h1)
+	})
+
+	server := http.Server{
+		Handler: m,
+	}
+
+	fmt.Println("Starting Server")
+	if err := server.Serve(socket); err != nil {
+		log.Fatal(err)
+	}
+
 }
